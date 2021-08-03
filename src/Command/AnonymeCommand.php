4dfb6f2280d3f2a1e8b566255c24e39b6b4d2415
Rodier <francisrodier78@yahoo.fr>
2021-08-03 19:08:17 +0200

    Réalisation de la commande app:anonyme

--- src/Command/AnonymeCommand.php
+++ src/Command/AnonymeCommand.php
@@ -4,44 +4,46 @@ namespace App\Command;
 
 use App\Entity\Task;
 use App\Entity\User;
-use App\Repository\TaskRepository;
-use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-  
-class AnonymousCommand extends Command
+
+
+class AnonymeCommand extends Command
 {
     // the name of the command (the part after "bin/console")
-    protected static $defaultName = 'app:anonymous';
-  
-    protected EntityManagerInterface $manager;
+    protected static $defaultName = 'app:anonyme';
 
-    protected function __construct(EntityManagerInterface $manager) 
+    //protected EntityManagerInterface $manager;
+    private EntityManagerInterface $manager;
+
+    //protected function __construct(EntityManagerInterface $manager) 
+    public function __construct(EntityManagerInterface $manager) 
     {
+        parent::__construct();
         $this->manager = $manager;    
     }
 
-    protected function configure()
+    protected function configure(): void
     {
         $this
             ->setDescription('Rattache les tâches anonymes à l\'utilisateur Anonymous.')
-            ->setHelp('Cette commande permet de rattacher des tâches sans utilisateurs à un utilisateur anonyme.');
+            ->setHelp('Cette commande permet de rattacher des tâches sans utilisateurs à un utilisateur anonymous.')
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
     }
-  
+
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-
-        //$this->manager 
-        //$em = $this->getDoctrine()->getManager();
-
-        //, UserRepository $repo1, TaskRepository $repo2
         $repo1 = $this->manager->getRepository(User::class);
         $user = $repo1->findOneBy(['username' => 'anonymous']);
 
         $repo2 = $this->manager->getRepository(Task::class);
-        $tasks = $repo2->findall(['author' => null]);
+        $tasks = $repo2->findBy(['author' => null]);
 
         $nbTasks = count($tasks);
         for ($i=0; $i < $nbTasks; $i++) { 
